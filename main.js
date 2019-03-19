e54e5a0d75d69709d647b7c60264dac60378b6f7
Quan Le <quanle@Quans-MacBook-Pro-2.local>
2019-03-19 16:29:29 -0700

    created google class in Maps instead of in main, added function to get coordinates

--- main.js
+++ main.js
@@ -2,28 +2,27 @@ $(document).ready(initializeApp);
 
 var results = {};
 var twitter = new TwitterLocation(results);
+var search = null;
+var coordinates = null;
 
 var orangeCountyCoordinates = {
     lat:33.67, lng:-117.78
 };
 
-var markers = null;
 var map = null;
 
 function initializeApp() {
     map = new Maps();
-
-    map.initMap();
-  
     var weather = new Weather();
     var yelp = new Yelp();
 }
 
 function clickHandler() {
-    // $('#submitSearch').on('click', )
+    $('#submitSearch').on('click', function() {
+        search = $('#submitSearch').text
+    })
     debugger;
     markers = new Maps();
 }
 
 var twitter = new TwitterLocation(results);
-
