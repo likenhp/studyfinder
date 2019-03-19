f01fa93bdbcc03ec9033344c9441d02056b23d3e
Kenneth Li <likenhp@gmail.com>
2019-03-19 12:50:10 -0700

    solved CORS issues

--- weather.js
+++ weather.js
@@ -1,28 +1,24 @@
 class Weather {
     constructor(){
-        this.handleClick = this.handleClick.bind(this);
         this.handleWeatherDataSuccess = this.handleWeatherDataSuccess.bind(this);
         this.handleWeatherDataError = this.handleWeatherDataError.bind(this);
-    }
-    eventHandlers(){
-        $(".weather").on("click", this.handleClick);
-    }
-    handleClick(){
-        this.handleWeatherData;
+        this.handleWeatherData();
     }
     handleWeatherData(){
         $.ajax({
-            url: "https://api.darksky.net/forecast/fe42c988ad036f09376551f306c17ae9/33.7175,-117.853104",
+            url: "darksky.php",
             dataType: "json",
             method: "get",
             success: this.handleWeatherDataSuccess,
-            error: this.handleWeatherDataError,
+            error: this.handleWeatherDataError
         })
     }
-    handleWeatherDataSuccess(){
-        console.log("it works");
+    handleWeatherDataSuccess(response){
+    console.log("it works", response);
+        
     }
-    handleWeatherDataError(){
-        console.log("it failed");
+    handleWeatherDataError(response){
+        console.log(response);
     }
 }
+
