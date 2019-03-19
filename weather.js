74aed043be7895702a4370538ee99e5f5d28ccc9
Kenneth Li <likenhp@gmail.com>
2019-03-19 11:19:20 -0700

    weather OOP finished creating Ajax Calls

--- weather.js
+++ weather.js
@@ -1,10 +1,28 @@
 class Weather {
     constructor(){
-
+        this.handleClick = this.handleClick.bind(this);
+        this.handleWeatherDataSuccess = this.handleWeatherDataSuccess.bind(this);
+        this.handleWeatherDataError = this.handleWeatherDataError.bind(this);
+    }
+    eventHandlers(){
+        $(".weather").on("click", this.handleClick);
+    }
+    handleClick(){
+        this.handleWeatherData;
     }
     handleWeatherData(){
         $.ajax({
-            
+            url: "https://api.darksky.net/forecast/fe42c988ad036f09376551f306c17ae9/33.7175,-117.853104",
+            dataType: "json",
+            method: "get",
+            success: this.handleWeatherDataSuccess,
+            error: this.handleWeatherDataError,
         })
+    }
+    handleWeatherDataSuccess(){
+        console.log("it works");
+    }
+    handleWeatherDataError(){
+        console.log("it failed");
     }
 }
