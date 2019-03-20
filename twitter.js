be6abc11f6ce56424a6d3d2522b05af79839117a
Kenneth Li <likenhp@gmail.com>
2019-03-20 12:56:50 -0700

    finished the scrolling issues

--- twitter.js
+++ twitter.js
@@ -4,7 +4,7 @@ class TwitterLocation {
         this.userID = '';
         this.info = {};
         this.includeEntities = '';
-
+        this.getDataSuccess = this.getDataSuccess.bind(this);
         this.getData();
     }
 
@@ -13,10 +13,25 @@ class TwitterLocation {
             url: 'https://s-apis.learningfuze.com/hackathon/twitter/index.php',
             dataType: 'json',
             method: '', 
-            data: {search_term: 'irvine', search_term: 'beer', },
-            success: function(result){
-                console.log(result);
-            }
+            data: {search_term: 'irvine', 
+            search_term: 'beer', },
+            success: this.getDataSuccess
         });
+    }
+    getDataSuccess(response){
+        debugger;
+        var twitterTextArray = response.tweets.statuses;
+        var tempTwitterArray = [];
+        console.log(twitterTextArray);
+        for(var tIndex = 0; tIndex < twitterTextArray.length; tIndex++){
+            var twitterText = response.tweets.statuses[tIndex].text;
+            tempTwitterArray.push(twitterText);
+            //var twitterDiv = $("<span>").append(twitterText);
+            //$(".twitterFeed").append(twitterDiv);
+        }
+        var twitterJoin = tempTwitterArray.join(" ");
+        var twitterSpan = $("<span>").append(twitterJoin);
+        $(".twitterFeed").append(twitterSpan);
+        //$(".twitterFeed").text(tempTwitterArray);
     }
 }
